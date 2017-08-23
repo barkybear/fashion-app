0d1146f13184615e406c0a89a4f850d55a2d73ac
Stephanie <idreamincolour@gmail.com>
2017-08-23 20:06:00 +0200

    get rid of # placeholder for links to get rid of console error

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,30 +3,30 @@ import './css/NavBar.css';
 
 const NavBar = () =>
   <nav className="nav">
-    <a className="nav__item" href="#">
+    <a className="nav__item" href="">
       luxe merken
     </a>
-    <a className="nav__item" href="#">
+    <a className="nav__item" href="">
       dames
     </a>
 
-    <a className="nav__item" href="#">
+    <a className="nav__item" href="">
       heren
     </a>
-    <a className="nav__item" href="#">
+    <a className="nav__item" href="">
       kinderen
     </a>
 
-    <a className="nav__item" href="#">
+    <a className="nav__item" href="">
       wonen & vrije tijd
     </a>
-    <a className="nav__item" href="#">
+    <a className="nav__item" href="">
       cadeaus
     </a>
-    <a className="nav__item" href="#">
+    <a className="nav__item" href="">
       outlet
     </a>
-    <a className="nav__item" href="#">
+    <a className="nav__item" href="">
       A-Z merken
     </a>
   </nav>;
